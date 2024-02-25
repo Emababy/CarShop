ccd16fb9d524c3f0d6c26feb1c4334d6fb0e61c4
Emababy <ziadembaby10@gmail.com>
2024-02-25 23:23:21 +0200

    second commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import HeroSection from "./HeroSection";
+import CustomButton from "./CustomButton";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+export {
+    HeroSection,
+    CustomButton,
+    NavBar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
+    ShowMore,
+};
